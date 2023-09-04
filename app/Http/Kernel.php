34f6c10ce0ac82c80fd1e38ce112b7aa54851c1f
Muhammad suleman <muhammad786suleman@gmail.com>
2023-09-04 21:47:47 +0500

    kernal file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        
+
         'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
         'isDriver'=> \App\Http\Middleware\DriverMiddleware::class,
     ];
